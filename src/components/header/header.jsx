3d3340e68b4c2be3a5ffaed0737d276a2cf03633
imgjeits <imgjeits@stud.ntnu.no>
2021-03-07 20:37:20 +0100

    moved header into separate component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './header.css';
+
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        {/* <li>
+                            <Link to="/dashboard">Dashboard</Link>
+                        </li> */}
+                        <li>
+                            <Link to="/login">Log in</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default Header
