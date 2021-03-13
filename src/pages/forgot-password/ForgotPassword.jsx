10b972f5db7d0b5c4cb982b1c77f55e56281a829
Ida <imgjeits@stud.ntnu.no>
2021-03-13 11:52:20 +0100

    added forms to login/forgotpass

--- src/pages/forgot-password/ForgotPassword.jsx
+++ src/pages/forgot-password/ForgotPassword.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 import './ForgotPassword.css'
 import { Link } from 'react-router-dom'
 
-import FormInput from '../../components/form-controls/form-input/form-input'
+import formValues from './forgotPassword.json'
+import Form from '../../components/form-controls/form/Form'
 
 class ForgotPassword extends Component {
     render() {
         return (
             <div className="ForgotPassword">
                 <h2>Get new password</h2>
-                <form action="#">
-                    <FormInput type="email" name="email" title="E-mail" />
-                </form>
+                <Form method="POST" action="#" inputs={formValues} />
+
                 <div>
                     <p>
                         New user? <Link to="/register">Register here</Link>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react'
+import { Link, Redirect } from "react-router-dom"
+import './Login.css'
 
-import { isLoggedIn } from '../../utils/isAuth';
-import './Login.css';
+import Form from '../../components/form-controls/form/Form'
+import { isLoggedIn } from '../../utils/isAuth'
+import formValues from './login.json'
 
 //This component is implementing more than one functionality only for academic purposes.
 //If the app is connected to a Backend, the auth logic should be implemented in a different file (SOLID)
 class Login extends Component {
-    state = { redirect: null };
+    constructor(props) {
+        super(props)
+        this.state = { redirect: null }
+    }
 
     componentDidMount() {
-        const isAuth = isLoggedIn();
-        this.setState({ isAuth });
+        const isAuth = isLoggedIn()
+        this.setState({ isAuth })
     }
 
     render() {
@@ -22,22 +27,34 @@ class Login extends Component {
 
         return (
             <div className="Login">
-                {!this.state.isAuth && <button onClick={this.handleLogIn}>Log In</button>}
-                {this.state.isAuth && <button onClick={this.handleLogOut}>Log Out</button>}
+                <h2>Log in</h2>
+                <Form method="GET" action="#" inputs={formValues} />
+
+                {/* {!this.state.isAuth && <button onClick={this.handleLogIn}>Log In</button>}
+                {this.state.isAuth && <button onClick={this.handleLogOut}>Log Out</button>} */}
+
+                <div>
+                    <p>
+                        New user? <Link to="/register">Register here</Link>
+                    </p>
+                    <p>
+                        <Link to="/forgot-password">Forgot password?</Link>
+                    </p>
+                </div>
             </div>
-        );
+        )
     }
 
     handleLogIn = () => {
         localStorage.setItem('userAuth', JSON.stringify(true));
         this.setState({ redirect: "/user" });
-    };
+    }
 
     handleLogOut = () => {
         localStorage.removeItem('userAuth');
         this.setState({ isAuth: false });
 
-    };
+    }
 }
 
 export default Login;
