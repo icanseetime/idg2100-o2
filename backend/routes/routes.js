47a1375a2afb3ee4bbd3c67c9ae713c911d83430
Ida <imgjeits@stud.ntnu.no>
2021-03-14 19:28:24 +0100

    comments in routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -6,10 +6,11 @@ const User = require('../models/User')
 // POST: Create new user
 const createUser = async (req, res) => {
     try {
+        // Generate secure password
         const salt = await bcrypt.genSalt()
         const securePass = await bcrypt.hash(req.body.matchingPasswords, salt)
-        console.log(securePass)
 
+        // Create user
         const newUser = new User({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
@@ -20,10 +21,10 @@ const createUser = async (req, res) => {
             availability: 'busy'
         })
 
+        // Save to DB & send response to client-side
         await newUser.save()
         let answer = { redirect: '/login' }
         res.status(201).json(answer)
-
     } catch (err) {
         res.status(500).json(`There was an error adding ${req.body.email} to the database. \n ${err} \n ${req}`)
     }
@@ -32,11 +33,12 @@ const createUser = async (req, res) => {
 // GET: Check if user exists by email
 const findUser = async (req, res) => {
     try {
+        // Find user by e-mail
         let user = await User.find({ email: req.query.email })
         if (user.length) {
             res.status(200).json({ successMessage: `An e-mail has been sent to ${req.query.email}. Please check your e-mail to reset your password.` })
         } else {
-            res.status(400).json({ errorMessage: `We couldn't fint a user with the e-mail address ${req.query.email}. Please try again.` })
+            res.status(400).json({ errorMessage: `We couldn't find a user with the e-mail address ${req.query.email}. Please try again.` })
         }
     } catch (err) {
         res.status(500).json(err)
@@ -49,7 +51,7 @@ const validateUser = async (req, res) => {
         // Check if user exists
         let user = await User.findOne({ email: req.body.email })
         if (user) {
-
+            // Check if input password matches encrypted user password
             if (await bcrypt.compare(req.body.password, user.password)) {
                 let answer = { localStorage: 'userAuth', redirect: '/user' }
                 res.status(200).json(answer)
